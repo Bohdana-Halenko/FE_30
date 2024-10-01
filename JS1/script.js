f1c7cf836ce9d5699075d2ed08677922d1f91032
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-10-01 21:08:58 +0300

    01.10

---
+++ JS1/script.js
@@ -0,0 +1,75 @@
+'use strict'
+
+// var - не використовуємо
+// let age = 20;
+// const x = 10000;
+// без ключового слова x = 10; дає помилку в суворому режимі
+
+
+// let username;
+// console.log(username);
+
+// age = 15;
+// console.log(age);
+
+
+// let age = 15;
+// age = 20;
+// console.log(age);
+
+
+// const age = 15;
+// console.log(age);
+
+// const priceOne = 10;
+// const quantity = 3;
+// let total = price1 * quantity;
+// console.log(total);
+// const priceNew = 50;
+
+
+// const x = 1;
+// const X = 1;
+
+// const COLOR = '#fff';
+
+
+// Типи даних
+// const age = 20; //number
+// const name = 'Bohdana'; //string
+// const newAge = '20';
+
+// const isModalOpen = true; //boolean
+// const isLogged = false;
+
+// // null
+
+// let username;
+// console.log(username); //undefined
+
+// let username;
+// console.log(typeof username); //undefined
+
+
+// const name = 'Bob';
+// console.log(typeof name); //string
+
+// const age = 15;
+// console.log(typeof age); //number
+
+// const isModalOpen = false;
+// console.log(typeof isModalOpen); //boolean
+
+
+// console
+// const name = 'Bohdana';
+// console.log('Username is ', name); //Username is  Bohdana
+
+// // const username = 'Hello, Bohdana';
+// // alert(username);
+
+// // const isComing = confirm('Please confirm hotel reservation');
+// // console.log(isComing);
+
+// const username = prompt('Введіть своє ім*я');
+// console.log(username);
