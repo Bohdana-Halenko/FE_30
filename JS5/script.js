b9d080d5640263865b0475d36f785a262baec740
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-10-29 20:02:53 +0200

    29.10

---
+++ JS5/script.js
@@ -0,0 +1,36 @@
+// let cost;
+// const sub = 'pro';
+
+// switch (sub) {
+//     case 'free':
+//         cost = 0;
+//         break;
+    
+//     case 'pro':
+//         cost = 200;
+//         break;
+    
+//     case 'premium':
+//         cost = 40;
+//         break;
+    
+//     default:
+//         console.log("Такого виду підписки не існує");
+// }
+
+// console.log(cost);
+
+
+// const value = 10;
+// console.log(value); //10
+
+// if (true) {
+//     console.log(value); //10
+// }
+
+// if (true) {
+//     const value = 5;
+//     console.log(value); //5
+// }
+
+// console.log(value); //ReferenceError: value is not defined
